fc8e51fd1734b88aaa4defce6cabba07f4d3cea5
Tarik Mekić <tmekic1@etf.unsa.ba>
2022-10-13 14:45:52 +0200

    new commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from '_services/account.service';
+import { User } from './_models/user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  title = 'Hey! This is my new headline.';
+  users: any;
+
+  constructor(private accountService: AccountService) {}
+
+  ngOnInit(): void {
+  this.setCurrentUser();
+  }
+
+  setCurrentUser() {
+    const user: User = JSON.parse(localStorage.getItem('user'));
+    this.accountService.setCurrentUser(user);
+  }
 }
